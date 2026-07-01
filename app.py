22b13ab41caaa1b7f12e1f3f436a952be0135d91
gulraina <gulraina2005@gmail.com>
2026-07-01 15:31:34 -0700

    flask todo app with add,delete,complete and css

--- app.py
+++ app.py
@@ -1,10 +1,28 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
-@app.route("/")
+tasks = []
+completed = []
+
+@app.route("/", methods=["GET", "POST"])
 def home():
-    return "Hello, DevOps!"
+    if request.method == "POST":
+        task = request.form["task"]
+        tasks.append(task)
+        completed.append(False)
+    return render_template("index.html", tasks=tasks, completed=completed)
+
+@app.route("/delete/<int:index>")
+def delete(index):
+    tasks.pop(index)
+    completed.pop(index)
+    return redirect("/")
+
+@app.route("/complete/<int:index>")
+def complete(index):
+    completed[index] = True
+    return redirect("/")
 
 if __name__ == "__main__":
     app.run(debug=True)
